25706b6663b43420da9a0c0abd318654616a0d04
JulCal <79907145+JulDagCal@users.noreply.github.com>
2024-12-03 18:11:06 +0100

    Add files via upload

---
+++ R_PEAKS/pantompkins.h
@@ -0,0 +1,29 @@
+#ifndef PanTompkins_H
+#define PanTompkins_H
+#include <QObject>
+#include <QDebug>
+#include <QVector>
+#include <QSharedPointer>
+class PanTompkins
+{
+public:
+    PanTompkins()=default;
+    /** @brief Funkcja do wykrywania szczytów R metodą Pana Tompkinsa.
+     *  @param electrocardiogram_signal - sygnał EKG
+     *  @param fs - częstotliwość próbkowania
+     *  @return Indeksy szczytów R
+     */
+    QVector<int> getPeaks(QSharedPointer<const QVector<float>> electrocardiogram_signal, int fs = 360);
+    QVector<float> normalize(QVector<float>& v) const;
+
+private:
+    int m_fs; // Częstotliwość próbkowania
+
+    QVector<float> filter(const QVector<float>& signal, float fc1, float fc2) const;
+
+
+    template<typename T>
+    QVector<T> conv(const QVector<T>& f, const QVector<T>& g) const;
+};
+
+#endif // PanTompkins_H
