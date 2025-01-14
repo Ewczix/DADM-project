9bf05cd465abe29a1d4145963a80a63d369ca879
EwaKrezel <130508277+EwaKrezel@users.noreply.github.com>
2025-01-14 23:34:04 +0100

    Update hrv_dfa.h

--- HRV_DFA/hrv_dfa.h
+++ HRV_DFA/hrv_dfa.h
@@ -1,14 +1,27 @@
 #ifndef HRV_DFA_H
 #define HRV_DFA_H
-
 #include <QVector>
 #include <utility>
+#include <QPair>
 
 class HRV_DFA
 {
+public:
+    // Konstruktor
+    HRV_DFA();
+    // DFA
+    void analyze(const QVector<double>& rr_intervals);
+    // alpha1 i alpha2
+    std::pair<double, double> getParams() const;
+    // Punkty
+    QPair<QVector<double>, QVector<double>> getPoints() const;
+
 private:
     double alpha1;
     double alpha2;
+
+    QVector<double> log_n1, log_F_n1;
+    QVector<double> log_n2, log_F_n2;
 
     const QVector<int> n1 = [] {
         QVector<int> temp;
@@ -25,13 +38,6 @@ private:
     double fluktuacja(const QVector<double>& y, int n);
     std::pair<double, double> polyfit(const QVector<double>& x, const QVector<double>& y);
 
-public:
-    // Konstruktor
-    HRV_DFA();
-    // DFA
-    void analyze(const QVector<double>& rr_intervals);
-    // alpha1 i alpha2
-    std::pair<double, double> getParams() const;
 };
 
 #endif // HRV_DFA_H
